4aea5687be065054841f5c173dfd2b9e2dfb0fe8
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-01-22 11:42:56 +0300

    feat: add subscription for new links

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const Link = require("./resolvers/Link");
 const User = require("./resolvers/User");
+const Subscription = require("./resolvers/Subscription");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   Link,
   User,
 };
