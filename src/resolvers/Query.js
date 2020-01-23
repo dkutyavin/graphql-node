ac681173aaef5386319aa0432479c067fb83b0f1
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-01-23 15:06:26 +0300

    feat: add filtration

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,14 @@
-const feed = (parent, args, context, info) => context.prisma.links();
+const feed = async (parent, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+  return await context.prisma.links({ where });
+};
 
 module.exports = {
   feed,
